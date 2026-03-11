de335744aee43256019437be9fcbcf7374bbeae1
John Maynard <jmaynard2@mtech.edu>
2026-03-11 11:25:57 -0600

    Add Convex hull function and test cases

--- main.py
+++ main.py
@@ -1,15 +1,104 @@
-from typing import List, Tuple
+"""
+John Maynard
+CSCI 332 Spring 2025
+Programming Assignment #class18
+I acknowledge that I have worked on this assignment independently, except where explicitly noted and referenced. Any collaboration or use of external resources has been properly cited. I am fully aware of the consequences of academic dishonesty and agree to abide by the university's academic integrity policy. I understand the importance the consequences of plagiarism.
+"""
+import sys
+type Point = tuple[float, float]
 
-Point = Tuple[float, float]
 
-def convex_hull_jarvis(points: List[Point]) -> List[Point]:
-   pass
+def convex_hull_jarvis(points: list[Point]) -> list[Point]:
+    hull: list[Point] = []
+    lenPoints: int = len(points)
+
+    # Get starting point (and test for errors)
+    xIter: int = 1
+    yIter: int = 1
+    leftMostIndex: int = 0
+    rightMostIndex: int = 0
+    upMostIndex: int = 0
+    downMostIndex: int = 0
+
+    for i in range(1, lenPoints):  # Skip first point
+        point: Point = points[i]
+
+        # Convoluted mess. If all the points are collinear then the main loop breaks.
+        # This is (theoretically) faster since it doesn't require looping through the points again to find the extremes.
+        if point[0] < points[leftMostIndex][0]:
+            leftMostIndex = i
+        elif point[0] > points[rightMostIndex][0]:
+            rightMostIndex = i
+
+        if point[1] < points[upMostIndex][1]:
+            upMostIndex = i
+        elif point[1] > points[downMostIndex][1]:
+            downMostIndex = i
+
+        # Confirms that all points are not collinear.
+        if point[0] == points[leftMostIndex][0]:
+            yIter += 1
+        if point[1] == points[leftMostIndex][1]:
+            xIter += 1
+
+    if xIter == lenPoints:
+        hull.append(points[leftMostIndex])
+        hull.append(points[rightMostIndex])
+        return hull
+    elif yIter == lenPoints:
+        hull.append(points[upMostIndex])
+        hull.append(points[downMostIndex])
+        return hull
+
+    # Default to leftmost.
+    hull.append(points[leftMostIndex])
+
+    # Create hull
+    nextPoint: Point
+    repeat: bool = True
+    currentIndex: int
+    while repeat:
+        # Check to make sure nextPoint != hull[-1], since orientation will always be zero if it is
+        currentIndex = points.index(hull[-1])
+        if currentIndex == lenPoints - 1:
+            nextPoint = points[currentIndex - 1]
+        else:
+            nextPoint = points[currentIndex + 1]
+
+        for j in points:
+            currentOrientation: float = getOrientation(hull[-1], nextPoint, j)
+            if currentOrientation < 0:
+                nextPoint = j
+
+        if nextPoint == hull[0]:
+            repeat = False
+            break
+
+        hull.append(nextPoint)
+
+    # Remove collinear points
+    removals: list[Point] = []
+    for i in range(len(hull)):
+        if getOrientation(hull[i - 1], hull[i], hull[(i + 1) % len(hull)]) == 0:
+            removals.append(hull[i])
+
+    if len(removals) < len(hull):
+        for i in removals:
+            _ = hull.remove(i)
+
+    return hull
+
+
+def getOrientation(point1: Point, point2: Point, point3: Point) -> float:
+    orientation: float = ((point2[1] - point1[1]) * (point3[0] - point2[0])) - (
+        (point3[1] - point2[1]) * (point2[0] - point1[0])
+    )
+    return orientation
 
 
 if __name__ == "__main__":
-    points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)] 
-    hull = convex_hull_jarvis(points) 
+    points: list[Point] = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)]
+    hull: list[Point] = convex_hull_jarvis(points)
     print("Convex Hull:", hull)
 
-
-#Convex Hull: [(0, 0), (0, 3), (3, 3), (3, 0)]
+# Convex Hull: [(0, 0), (0, 3), (3, 3), (3, 0)]
--- tests.py
+++ tests.py
@@ -1,32 +1,228 @@
+"""
+John Maynard
+CSCI 332 Spring 2025
+Programming Assignment #class18
+I acknowledge that I have worked on this assignment independently, except where explicitly noted and referenced. Any collaboration or use of external resources has been properly cited. I am fully aware of the consequences of academic dishonesty and agree to abide by the university's academic integrity policy. I understand the importance the consequences of plagiarism.
+"""
+
 import unittest
-from main import convex_hull_jarvis
+
+from main import Point, convex_hull_jarvis
 
 
 class TestMathFunctions(unittest.TestCase):
-    
+
+    # Base testing method
+    def baseTestMethod(self, points: list[Point], validResults: list[Point]):
+        hull: list[Point] = convex_hull_jarvis(points)
+
+        for result in validResults:
+            self.assertIn(result, hull)
+
+        for point in points:
+            if point not in validResults:
+                self.assertNotIn(point, hull)
+
+    # Basic tests
     def test_example_from_prompt(self):
-        exit(1)
+        points: list[Point] = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)]
+        resultValid: list[Point] = [(0, 3), (0, 0), (3, 0), (3, 3)]
+        self.baseTestMethod(points, resultValid)
 
-    def test_square_with_inner_points(self):
-        exit(1)
+    def test_basic1(self):
+        points: list[Point] = [
+            (0, 3),
+            (2, 2),
+            (1, 1),
+            (2, 1),
+            (3, 0),
+            (0, 0),
+            (3, 3),
+            (2, -1),
+        ]
+        resultValid: list[Point] = [(0, 3), (0, 0), (3, 0), (3, 3), (2, -1)]
+        self.baseTestMethod(points, resultValid)
 
-    def test_duplicates(self):
-        exit(1)
+    def test_basic2(self):
+        points: list[Point] = [
+            (0, 3),
+            (2, 2),
+            (1, 1),
+            (2, 1),
+            (3, 0),
+            (0, 0),
+            (3, 3),
+            (2, -1),
+            (1, -0.5),
+        ]
+        resultValid: list[Point] = [(0, 3), (0, 0), (3, 0), (3, 3), (2, -1)]
+        self.baseTestMethod(points, resultValid)
 
-    def test_all_collinear(self):
-        exit(1)
+    def test_basic3(self):
+        points: list[Point] = [
+            (0, 3),
+            (2, 2),
+            (1, 1),
+            (2, 1),
+            (3, 0),
+            (0, 0),
+            (3, 3),
+            (2, -1),
+            (1, -0.5),
+            (0, 2),
+            (0, 1),
+        ]
+        resultValid: list[Point] = [
+            (0, 3),
+            (0, 0),
+            (3, 0),
+            (3, 3),
+            (2, -1),
+        ]
+        self.baseTestMethod(points, resultValid)
 
-    def test_two_points(self):
-        exit(1)
+    def test_basic4(self):
+        points: list[Point] = [
+            (0, 3),
+            (2, 2),
+            (1, 1),
+            (2, 1),
+            (3, 0),
+            (0, 0),
+            (3, 3),
+            (2, -1),
+            (1, -0.5),
+            (0, 2),
+            (0, 1),
+            (-1, 1),
+        ]
+        resultValid: list[Point] = [
+            (0, 3),
+            (0, 0),
+            (3, 0),
+            (3, 3),
+            (2, -1),
+            (-1, 1),
+        ]
+        self.baseTestMethod(points, resultValid)
 
-    def test_single_point(self):
-       exit(1)
+    def test_basic5(self):
+        points: list[Point] = [
+            (2, 0),
+            (0, 2),
+            (9, 5),
+            (6, 4),
+            (2, 1),
+            (4, 5),
+            (3, -2),
+            (7, 0),
+            (5, 1)
+        ]
+        resultValid: list[Point] = [
+            (0, 2),
+            (3, -2),
+            (7, 0),
+            (9, 5),
+            (4, 5)
+        ]
+        self.baseTestMethod(points, resultValid)
 
-    def test_triangle(self):
-        exit(1)
+    def test_basic6(self):
+        points: list[Point] = [
+            (0, 0),
+            (1, 1),
+            (0, 2)
+        ]
+        resultValid: list[Point] = [
+            (0, 0),
+            (1, 1),
+            (0, 2)
+        ]
+        self.baseTestMethod(points, resultValid)
 
-    def test_collinear_on_edges(self):
-        exit(1)
+    # Edge cases
+    def test_edge1(self):
+        points: list[Point] = [
+            (2, 0),
+            (0, 2),
+            (9, 5),
+            (6, 4),
+            (2, 1),
+            (4, 5),
+            (3, -2),
+            (7, 0),
+            (5, 1),
+            (2, 0),
+            (0, 2)
+        ]
+        resultValid: list[Point] = [
+            (0, 2),
+            (3, -2),
+            (7, 0),
+            (9, 5),
+            (4, 5)
+        ]
+        self.baseTestMethod(points, resultValid)
+
+    def test_edge2(self):
+        points: list[Point] = [
+            (2, 0),
+            (1, 0),
+            (0, 0),
+            (3, 0),
+            (4, 0)
+        ]
+        resultValid: list[Point] = [
+            (4, 0),
+            (0, 0)
+        ]
+        self.baseTestMethod(points, resultValid)
+
+    def test_edge3(self):
+        points: list[Point] = [
+            (2, 1),
+            (6, 2)
+        ]
+        resultValid: list[Point] = [
+            (2, 1),
+            (6, 2)
+        ]
+        self.baseTestMethod(points, resultValid)
+
+    def test_edge4(self):
+        points: list[Point] = [
+            (2, 0),
+            (6, 0)
+        ]
+        resultValid: list[Point] = [
+            (2, 0),
+            (6, 0)
+        ]
+        self.baseTestMethod(points, resultValid)
+
+    def test_edge5(self):
+        points: list[Point] = [
+            (2, 0)
+        ]
+        resultValid: list[Point] = [
+            (2, 0)
+        ]
+        self.baseTestMethod(points, resultValid)
+
+    def test_edge6(self):
+        points: list[Point] = [
+            (2, 0),
+            (2, 0),
+            (2, 0),
+            (2, 0),
+            (2, 0),
+            (2, 0)
+        ]
+        resultValid: list[Point] = [
+            (2, 0)
+        ]
+        self.baseTestMethod(points, resultValid)
+
 
 if __name__ == "__main__":
-    unittest.main()
+    _ = unittest.main()
